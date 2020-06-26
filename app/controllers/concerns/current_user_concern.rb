58d934d7f8c0a63ff88ab9761c538b4327b57f88
mayalexandera <maya.blevins@gmail.com>
2020-06-26 19:37:22 -0400

    added guest user for current_user

---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,18 @@
+module CurrentUserConcern
+  extend ActiveSupport::Concern
+
+    #overriding devise's current_user method
+    #allowing for guest users to visit the site
+    def current_user
+    super || guest_user
+  end
+
+  def guest_user
+    OpenStruct.new(
+      name: "Guest User", 
+      first_name: "Guest", 
+      last_name: "User", 
+      email: "guest@example.com"
+    )
+  end
+end
