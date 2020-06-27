7c6d0c1a961167e30d5b90c0fb139d1bff1b486a
mayalexandera <maya.blevins@gmail.com>
2020-06-26 21:33:06 -0400

    implemented view helper for auth links

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -1,18 +1,17 @@
 module CurrentUserConcern
   extend ActiveSupport::Concern
-
     #overriding devise's current_user method
     #allowing for guest users to visit the site
     def current_user
-    super || guest_user
-  end
+      super || guest_user
+    end
 
-  def guest_user
-    OpenStruct.new(
-      name: "Guest User", 
-      first_name: "Guest", 
-      last_name: "User", 
-      email: "guest@example.com"
-    )
-  end
+    def guest_user
+      OpenStruct.new(
+        name: "Guest User", 
+        first_name: "Guest", 
+        last_name: "User", 
+        email: "guest@example.com"
+      )
+    end
 end
