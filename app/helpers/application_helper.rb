3583a091f6400d3799b570606b4e921be4b4658f
mayalexandera <maya.blevins@gmail.com>
2020-06-27 15:40:55 -0400

    integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class:"source-greeting")
    end 
   end
+
+  def copyright_generator
+    MayalexanderaViewTool::Renderer.copyright "Maya Alexandera", "All rights reserved"
+  end
 end
